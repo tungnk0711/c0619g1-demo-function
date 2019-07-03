78f0b7312740576cff1b74cffb9d5aa21f0a8a25
nguyenkhanhtung <tungnk0711@gmail.com>
2019-07-03 11:35:25 +0700

    demo function

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const EPSILON = 1E-14;
-var x = 1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1;
-if (Math.abs(x - 0.3) < EPSILON)
-    console.log(x + " is approximately 0.5");
-else {
-    console.log("false");
+function pushArr() {
+    arr.push(str);
+    return arr;
 }
